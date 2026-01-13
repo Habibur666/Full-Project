67d651bb6493f5f41a096037e6872a30db6db3ab
habibur rahaman <mdhabiburrahaman1962005@gmail.com>
2026-01-13 21:31:36 +0530

    Fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User=require("../models/user.js");
+const { body, validationResult } = require("express-validator");
 
 module.exports.renderSignupUser=(req,res)=>{
   res.render("users/signup.ejs");
@@ -6,16 +7,33 @@ module.exports.renderSignupUser=(req,res)=>{
 
 module.exports.signupUser=async (req,res,next)=>{ 
   try{
-  let {username, email, password}=req.body;
-  let newUser=new User({username, email});
-  let registeredUser=await User.register(newUser, password);
-  req.login(registeredUser, (err)=>{
-    if(err) return next(err);
-    req.flash("success", "Welcome to Wanderlust!");
-    res.redirect("/listings");
-  });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("error", "Invalid input. Please check your details.");
+      return res.redirect("/signup");
+    }
+
+    let {username, email, password}=req.body;
+    if (!username || !email || !password) {
+      req.flash("error", "All fields are required.");
+      return res.redirect("/signup");
+    }
+
+    let newUser=new User({username, email});
+    let registeredUser=await User.register(newUser, password);
+
+    req.login(registeredUser, (err)=>{
+      if(err){
+        console.error("Login error:", err);
+        req.flash("error", "Login failed. Please try again.");
+        return res.redirect("/login");
+      }
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    });
   
   } catch(e){
+    console.error("Signup error:", e);
     req.flash("error", e.message);
     res.redirect("/signup");
   }
